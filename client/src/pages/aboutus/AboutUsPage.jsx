91ff4e25db3a9a1af8c5ab928b7980c883fb9c94
marm <marm.rikhav@gmail.com>
2026-09-24 13:26:42 +0530

    feat: pass onNavigate prop to AboutUsComponent in AboutUsPage

--- client/src/pages/aboutus/AboutUsPage.jsx
+++ client/src/pages/aboutus/AboutUsPage.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import AboutUsComponent from '../../components/aboutus/AboutUsComponent';
 
-const AboutUsPage = () => {
-  return <AboutUsComponent />;
+const AboutUsPage = ({ onNavigate }) => {
+  return <AboutUsComponent onNavigate={onNavigate} />;
+};
+
+AboutUsPage.propTypes = {
+  onNavigate: PropTypes.func,
 };
 
 export default AboutUsPage;
