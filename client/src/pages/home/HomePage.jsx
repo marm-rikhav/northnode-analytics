558870df799b9929de7cbb7862a0c844b3023561
marm <marm.rikhav@gmail.com>
2026-09-24 13:13:47 +0530

    feat: add onNavigate prop to HomePage and define prop-types validation

--- client/src/pages/home/HomePage.jsx
+++ client/src/pages/home/HomePage.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import HomeComponent from '../../components/home/HomeComponent';
 
-const HomePage = () => {
-  return <HomeComponent />;
+const HomePage = ({ onNavigate }) => {
+  return <HomeComponent onNavigate={onNavigate} />;
+};
+
+HomePage.propTypes = {
+  onNavigate: PropTypes.func,
 };
 
 export default HomePage;
