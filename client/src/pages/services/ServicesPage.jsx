2a8b40da7885ea723352a1f6e7290a64ddb6a5f9
marm <marm.rikhav@gmail.com>
2026-09-24 13:36:48 +0530

    feat: pass onNavigate prop to ServicesComponent in ServicesPage

--- client/src/pages/services/ServicesPage.jsx
+++ client/src/pages/services/ServicesPage.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import ServicesComponent from '../../components/services/ServicesComponent';
 
-const ServicesPage = () => {
-  return <ServicesComponent />;
+const ServicesPage = ({ onNavigate }) => {
+  return <ServicesComponent onNavigate={onNavigate} />;
+};
+
+ServicesPage.propTypes = {
+  onNavigate: PropTypes.func,
 };
 
 export default ServicesPage;
