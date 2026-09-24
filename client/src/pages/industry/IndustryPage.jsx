530ee2d3606de32031cf7717df80ffd65bae8e37
marm <marm.rikhav@gmail.com>
2026-09-24 13:43:59 +0530

    feat: pass onNavigate prop to IndustryComponent in IndustryPage

--- client/src/pages/industry/IndustryPage.jsx
+++ client/src/pages/industry/IndustryPage.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import IndustryComponent from '../../components/industry/IndustryComponent';
 
-const IndustryPage = () => {
-  return <IndustryComponent />;
+const IndustryPage = ({ onNavigate }) => {
+  return <IndustryComponent onNavigate={onNavigate} />;
+};
+
+IndustryPage.propTypes = {
+  onNavigate: PropTypes.func,
 };
 
 export default IndustryPage;
