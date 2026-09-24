9dffe4324af35990eec93964afb4bd796e2a091b
marm <marm.rikhav@gmail.com>
2026-09-24 12:49:02 +0530

    feat: add prop-types validation to HeaderPage component

--- client/src/pages/header/HeaderPage.jsx
+++ client/src/pages/header/HeaderPage.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import HeaderComponent from '../../components/header/HeaderComponent';
 
 const HeaderPage = ({ activeTab, onSelectTab }) => {
   return <HeaderComponent activeTab={activeTab} onSelectTab={onSelectTab} />;
+};
+
+HeaderPage.propTypes = {
+  activeTab: PropTypes.string,
+  onSelectTab: PropTypes.func,
 };
 
 export default HeaderPage;
