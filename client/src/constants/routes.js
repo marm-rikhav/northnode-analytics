4d9e0f7f0003edce419c4bbe9b8e74c8d52cd9a3
marm <marm.rikhav@gmail.com>
2026-09-26 13:08:46 +0530

    feat: add ROUTES and ROUTE_MAP constants for application navigation

---
+++ client/src/constants/routes.js
@@ -0,0 +1,15 @@
+export const ROUTES = {
+  HOME: '/',
+  SERVICES: '/services',
+  ABOUT_US: '/about-us',
+  INDUSTRY: '/industry',
+  CONTACT_US: '/contact-us',
+};
+
+export const ROUTE_MAP = {
+  Home: ROUTES.HOME,
+  'About Us': ROUTES.ABOUT_US,
+  Services: ROUTES.SERVICES,
+  Industry: ROUTES.INDUSTRY,
+  'Contact Us': ROUTES.CONTACT_US,
+};
